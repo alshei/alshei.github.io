1667f8075287f5f773310f9ad70ddc4292a384c1
alsheikhiu <alsheikh@iu.edu>
2022-05-22 17:41:36 -0400

    Scroll to home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,7 +2,10 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div className="bg-light1 dark:bg-dark1 w-screen h-screen -mt-6"></div>
+    <div
+      id="home"
+      className="bg-light1 dark:bg-dark1 w-screen h-screen -mt-6"
+    ></div>
   );
 };
 
